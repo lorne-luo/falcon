4ada5108955f6f2f34c71133cc03f695e5a4f26f
Lorne <dev@luotao.net>
2019-11-15 19:59:24 +1100

    fix: update TimeFrameEvent param

--- falcon/event.py
+++ falcon/event.py
@@ -48,10 +48,10 @@ class DebugEvent(BaseEvent):
 class TimeFrameEvent(BaseEvent):
     type = EventType.TIMEFRAME
 
-    def __init__(self, timeframe, current, previous, timezone, time):
+    def __init__(self, timeframe, current_time, previous, timezone, time):
         super(TimeFrameEvent, self).__init__()
         self.timeframe = timeframe
-        self.current = current
+        self.current_time = current_time
         self.previous = previous
         self.timezone = timezone
         self.time = time
